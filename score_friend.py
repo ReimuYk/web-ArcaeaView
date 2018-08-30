2c3ce6f5875859dd77182f130fe7e4635140c1e2
yuki.h <yuki.h@sjtu.edu.cn>
2018-08-30 22:34:35 +0800

    add delete find(friend

--- score_friend.py
+++ score_friend.py
@@ -3,15 +3,7 @@ s = requests.Session()
 import warnings
 warnings.filterwarnings('ignore')
 
-header = {
-    "Accept-Encoding":"identity",
-    "Content-Type":r"application/x-www-form-urlencoded; charset=utf-8",
-    "Authorization":"Bearer Clg3N0Og8ngL6+KK4a10IT0rmma9Ft89cZxYDTzb6zQ=",
-    "AppVersion":"1.7.2c",
-    "User-Agent":r"Dalvik/1.6.0 (Linux; U; Android 4.4.4; G750-T20 Build/KTU84P)",
-    "Host":"arcapi.lowiro.com",
-    "Connection":"Keep-Alive"
-    }
+
 
 songid = "grievouslady"
 url = r"https://arcapi.lowiro.com/4/score/song/friend?song_id=%s&difficulty=1&start=0&limit=10"%songid
@@ -22,22 +14,80 @@ def loadsongs():
     f.close()
     return eval(t)
 
+def getdata():
+    res = {}
+    header = {
+        "Accept-Encoding":"identity",
+        "Content-Type":r"application/x-www-form-urlencoded; charset=utf-8",
+        "Authorization":"Bearer Clg3N0Og8ngL6+KK4a10IT0rmma9Ft89cZxYDTzb6zQ=",
+        "AppVersion":"1.7.2c",
+        "User-Agent":r"Dalvik/1.6.0 (Linux; U; Android 4.4.4; G750-T20 Build/KTU84P)",
+        "Host":"arcapi.lowiro.com",
+        "Connection":"Keep-Alive"
+        }
+    songlist = loadsongs()
+    cons = ["name","song_id","difficulty","score","shiny_perfect_count","perfect_count","near_count","miss_count","rating"]
+    for song in songlist:
+        for i in range(3):
+            url = r"https://arcapi.lowiro.com/4/score/song/friend?song_id=%s&difficulty=%d&start=0&limit=10"%(song["songid"],i)
+            print((song["songid"],i))
+            resp = s.get(url,headers=header,verify=False)
+            resp = eval(resp.text.replace("true","True").replace("false","False"))
+            for userdata in resp["value"]:
+                line = []
+                for c in cons:
+                    line.append(userdata[c])
+                try:
+                    res[userdata["name"]].append(line)
+                except:
+                    res[userdata["name"]]=[line]
+    for user in list(res):
+        res[user] = sorted(res[user],key=lambda u:u[8],reverse=True)
+    return res
 
-##resp = s.get(url,headers=header,verify=False)
-##print(resp.text)
-songlist = loadsongs()
-cons = ["name","song_id","difficulty","score","shiny_perfect_count","perfect_count","near_count","miss_count","rating"]
-out = open("output.csv","w")
-for song in songlist:
-    for i in range(3):
-        url = r"https://arcapi.lowiro.com/4/score/song/friend?song_id=%s&difficulty=%d&start=0&limit=10"%(song["songid"],i)
-        print((song["songid"],i))
-        resp = s.get(url,headers=header,verify=False)
-        resp = eval(resp.text.replace("true","True").replace("false","False"))
-        for userdata in resp["value"]:
-            for c in cons:
-                out.write(str(userdata[c]))
-                out.write(',')
-            out.write('\n')
-out.close()
+def addfriend(uid):
+    url = r"https://arcapi.lowiro.com/4/friend/me/add"
+    header = {
+        "Accept-Encoding":"identity",
+        "User-Agent":r"Dalvik/1.6.0 (Linux; U; Android 4.4.4; G750-T20 Build/KTU84P)",
+        "Content-Length":str(len(uid)+12),
+        "Content-Type":r"application/x-www-form-urlencoded; charset=utf-8",
+        "AppVersion":"1.7.2c",
+        "Authorization":"Bearer Clg3N0Og8ngL6+KK4a10IT0rmma9Ft89cZxYDTzb6zQ=",
+        "Host":"arcapi.lowiro.com",
+        "Connection":"Keep-Alive"
+        }
+    body = "friend_code="+uid
+    resp = s.post(url,headers=header,data=body.encode(),verify=False)
+    print(resp.text)
+def deletefriend(fid):
+    url = r"https://arcapi.lowiro.com/4/friend/me/delete"
+    header = {
+        "Accept-Encoding":"identity",
+        "User-Agent":r"Dalvik/1.6.0 (Linux; U; Android 4.4.4; G750-T20 Build/KTU84P)",
+        "Content-Length":str(len(fid)+10),
+        "Content-Type":r"application/x-www-form-urlencoded; charset=utf-8",
+        "AppVersion":"1.7.2c",
+        "Authorization":"Bearer Clg3N0Og8ngL6+KK4a10IT0rmma9Ft89cZxYDTzb6zQ=",
+        "Host":"arcapi.lowiro.com",
+        "Connection":"Keep-Alive"
+        }
+    body = "friend_id="+fid
+    resp = s.post(url,headers=header,data=body.encode(),verify=False)
+    print(resp.text)
+def getfriendlist():
+    url = r"https://arcapi.lowiro.com/4/compose/aggregate?calls=%5B%7B%20%22endpoint%22%3A%20%22user%2Fme%22%2C%20%22id%22%3A%200%20%7D%2C%20%7B%20%22endpoint%22%3A%20%22purchase%2Fbundle%2Fpack%22%2C%20%22id%22%3A%201%20%7D%5D"
+    header = {
+        "Accept-Encoding":"identity",
+        "Content-Type":r"application/x-www-form-urlencoded; charset=utf-8",
+        "Authorization":"Bearer Clg3N0Og8ngL6+KK4a10IT0rmma9Ft89cZxYDTzb6zQ=",
+        "AppVersion":"1.7.2c",
+        "User-Agent":r"Dalvik/1.6.0 (Linux; U; Android 4.4.4; G750-T20 Build/KTU84P)",
+        "Host":"arcapi.lowiro.com",
+        "Connection":"Keep-Alive"
+        }
+    resp = s.get(url,headers=header,verify=False)
+    resp = eval(resp.text.replace("true","True").replace("false","False"))
+    return resp["value"][0]["value"]["friends"]
+fl = getfriendlist()
 
